132034d4d7816c96a5a051b9007270ce3a0c2264
Soumen3 <soumensamanta112233@gmail.com>
2024-06-07 12:41:39 +0530

    login page design partly complete

--- diary/urls.py
+++ diary/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
 	path("login/", views.user_login, name="login"),
 	path("logout/", views.user_logout, name="logout"),
-	path("", views.home, name="home"),
 	path("signup/", views.user_signup, name="signup"),
+	path("", views.home, name="home"),
+	path("about/", views.about, name="about"),
+	path("my-diary/", views.myDairy, name="myDiary"),
+	path("plans/", views.plans, name="plans")
 ]
