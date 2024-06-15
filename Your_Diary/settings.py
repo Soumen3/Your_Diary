7e43bf3c6e2d2d45d1b2ca5984e37f0f151c477a
Soumen3 <soumensamanta112233@gmail.com>
2024-06-16 02:10:29 +0530

    invalid url handled

--- Your_Diary/settings.py
+++ Your_Diary/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_11k07ho#okb+cn%d)*6y1i!mo(@bztinh1!@mfr+1)u7ye3za'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 	'diary',
 	'verify_email.apps.VerifyEmailConfig',
 	'widget_tweaks',
-	'admin_honeypot',
+	'admin_honeypot'
 ]
 
 MIDDLEWARE = [
@@ -122,9 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
